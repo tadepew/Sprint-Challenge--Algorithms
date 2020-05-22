4d38be673dd2d77bd5a591de82bc1b26ae715beb
Tristan Depew <tristan.depew@gmail.com>
2020-05-22 13:46:17 -0400

    finished mvp

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -1,15 +1,14 @@
-#### Please add your answers to the ***Analysis of  Algorithms*** exercises here.
+#### Please add your answers to the **_Analysis of Algorithms_** exercises here.
 
 ## Exercise I
 
-a)
+a)O(n) linear, while loops runs n times.
 
+b) O(n log n) the inner while loop j is increasing exponentially and the outer for loop runs n times
 
-b)
-
-
-c)
+c) O(log n) the recursive call is directly proportional to the n
 
 ## Exercise II
 
-
+I would use a binary search tree (O(log n)) to reduce possibilites by half each time until it is at floor f in which it doesn't break on that floor. It would go left(lower floor) if the egg breaks at that midpoint and go right if the egg doesn't break. When it is at the end the egg will not break at floor f but will break at the floor below.
+Logn
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -93,18 +95,64 @@ class SortingRobot:
         return self._light == "ON"
 
     def sort(self):
-        """
-        Sort the robot's list.
-        """
-        # Fill this out
-        pass
+        # while self.can_move_right():
+        #     # maybe need a for loop inside to see the next smallest number
+        #     # while there is somewere to the right to go
+        #     #
+        #     # item in hand
+        #     if self.compare_item() is None:
+        #         self.swap_item()
+        #     self.move_right()
+        #     # if held item is less than item at position, swap
+        #     if self.compare_item() == -1:
+        #         self.swap_item()
+        #     else:
+        #         self.move_right()
+
+        # while self.can_move_left():
+        #     if self.compare_item() == 1:
+        #         self.swap_item()
+        #         self.move_left()
+        #     else:
+        #         self.move_left()
+
+        # if self.can_move_left() is False:
+        #     self.swap_item()
+
+        # return self._list
+
+        # QUICKSORT
+        self.set_light_on()
+
+        while self.light_is_on():
+            # turn light off
+            self.set_light_off()
+            # pick up first item, putting down "none"
+            self.swap_item()
+            # move right, comparing each item to one held
+            while self.can_move_right():
+                self.move_right()
+                # if robot is holding the larger item
+                if self.compare_item() == 1:
+                    # pick up smaller item
+                    self.swap_item()
+                    # turn on light
+                    self.set_light_on()
+            # Now at far right of list
+            # Move to far left
+            while self.compare_item() is not None:
+                self.move_left()
+            # Place smallest item at beginning
+            self.swap_item()  # Holding "none"
+            self.move_right()
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
-
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [3, 9, 5, 2, 1]
+    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+    #      45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
